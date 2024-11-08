141784a4a22d0f0b0050561c0505ca86b6b77fad
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2024-11-08 17:47:20 +0000

    Reduce tab size

--- src/Hello.cs
+++ src/Hello.cs
@@ -1,7 +1,7 @@
 using System;
 
 class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
-    }
+  static void Main(string[] args) {
+    Console.WriteLine("Hello, World!");
+  }
 }
